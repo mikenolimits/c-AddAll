e7f4d25a3c375072d2c8e21d73a7c8e0419d8fd5
mikenolimits <mkantor100@qc.cuny.edu>
2014-11-17 14:48:01 -0500

    downbytwo

--- lab13.cpp
+++ lab13.cpp
@@ -2,13 +2,24 @@
 using namespace std;
 
 
+
+   int addDownTwo(int& n, int& total){
+
+    if(n % 2){
+     n =  n - 2;
+     total = total + n;
+     addDownTwo(n);
+   }
+
+   return total; 
+   }
+
    void addAll(int& n, int& x){
 
-     n++;
 
      cout<<x;
 
-     if(x <=  n){
+     if(x < n){
         x++;
         addAll(n,x);
      }
@@ -20,11 +31,17 @@ using namespace std;
 
      int n;
      int x = 1;
+     int total = 0;
+
 
      cout<<"please enter a number"<<endl;
      cin>>n;
 
       addAll(n,x);
+
+      total = addDownTwo(n,total);
+      
+      cout<<total;
 
       return 0;
    }
